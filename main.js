b10b190b8f456ede23ff455af7011cf7ef0bebad
Maikel Ceolin <92675662+maikelceolin@users.noreply.github.com>
2022-11-02 23:42:25 -0300

    sync

--- main.js
+++ main.js
@@ -22,16 +22,16 @@ function createCard (nation1, nation2, date, day, hour, score1, score2){
   `
 }
 
+function setGame(){
+  document.querySelector("#app").innerHTML = `
+      <header>
+        <img src="./assets/logo.svg" alt="Logo da NLW">
+      </header>
+      <main>
+        ${createCard("bra", "srb", "24/11", "quinta", "16:00", "", "")}
+        ${createCard("bra", "sui", "28/11", "segunda", "13:00", "", "")}
+        ${createCard("cmr", "bra", "02/12", "sexta", "16:00", "", "")}
+      </main>`
+}
 
-document.querySelector("#app").innerHTML = `
-    <header>
-      <img src="./assets/logo.svg" alt="Logo da NLW">
-    </header>
-    <main>
-      ${createCard("bra", "srb", "24/11", "quinta", "16:00", "", "")}
-      ${createCard("bra", "sui", "28/11", "segunda", "13:00", "", "")}
-      ${createCard("cmr", "bra", "02/12", "sexta", "16:00", "", "")}
-    </main>
-   
-
-`
+window.onload = setGame()
