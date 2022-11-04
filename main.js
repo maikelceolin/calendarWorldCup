57c7f3899c1aa3f2c0dc13f7f5a2d6a2caed32c4
maikelceolin <maikelceolin@gmail.com>
2022-11-04 16:16:16 -0300

    sync

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-function setGame (nation1, nation2, score1, score2){
+function setGame (hour, nation1, nation2, score1, score2){
   return `
   <li>
     <div class="nation">
-      <img src="./assets/flags/flag-${nation1}.svg" alt="Bandeira do ${nation1}">
+      <img src="./assets/flags/flag-${nation1}.svg" alt="${nation1}">
       <p>${nation1}</p>
     </div>
-    <strong>${score1} <span>x</span> ${score2}</strong>
+    <div>
+      <div id="hour"><span>${hour}</span></div>
+      <strong>${score1} <span>x</span> ${score2}</strong>
+    </div>
+    
     <div class="nation">
-      <img src="./assets/flags/flag-${nation2}.svg" alt="Bandeira da ${nation2}">
+      <img src="./assets/flags/flag-${nation2}.svg" alt="${nation2}">
       <p>${nation2}</p>
     </div>
   </li>
@@ -15,34 +19,87 @@ function setGame (nation1, nation2, score1, score2){
 }
 
 
-function createCard (date, day, nation1, nation2, nation3, nation4, score1, score2, score3, score4){
+function createCard (date, day, nation1, nation2, hour1, nation3, nation4, hour2, score1, score2, score3, score4){
   document.querySelector("main").innerHTML +=`
     <section id="cards">
       <div class="card">
         <h2> ${date} <span> ${day}</span></h2>
         <ul>
-            ${setGame(nation1, nation2, score1, score2)}
-            ${setGame(nation3, nation4, score3, score4)}
+            ${setGame(hour1, nation1, nation2, score1, score2)}
+            ${setGame(hour2, nation3, nation4, score3, score4)}
         </ul>
       </div>
     </section>
     `
 }
 
-function showGames (){
+function grupoA(){
 
-// jogo 24/11
+  createCard("20/11","domingo","sui","cmr","9:00","bra","srb","16h00","0","0","0","0")
+  createCard("28/11","segunda","cmr","srb","7h00","bra","sui","13h00","0","0","0","0")
+  createCard("02/12","sexta","srb","sui","16h00","cmr","bra","16h00","0","0","0","0")
 
-createCard("24/11","quinta","sui","cmr","bra","srb","0","0","0","0")
+}
 
-// jogo Brasil x Servia
-createCard("28/11","segunda","cmr","srb","bra","sui","0","0","0","0")
+function grupoB(){
 
-// jogo Brasil x Suica
-createCard("02/12","sexta","srb","sui","cmr","bra","0","0","0","0")
-
+  createCard("20/11","domingo","sui","cmr","9:00","bra","srb","16h00","0","0","0","0")
+  createCard("28/11","segunda","cmr","srb","7h00","bra","sui","13h00","0","0","0","0")
+  createCard("02/12","sexta","srb","sui","16h00","cmr","bra","16h00","0","0","0","0")
 
 }
 
 
-window.onload = showGames()
+function grupoC(){
+
+  createCard("20/11","domingo","sui","cmr","9:00","bra","srb","16h00","0","0","0","0")
+  createCard("28/11","segunda","cmr","srb","7h00","bra","sui","13h00","0","0","0","0")
+  createCard("02/12","sexta","srb","sui","16h00","cmr","bra","16h00","0","0","0","0")
+
+}
+
+
+function grupoD(){
+
+  createCard("20/11","domingo","sui","cmr","9:00","bra","srb","16h00","0","0","0","0")
+  createCard("28/11","segunda","cmr","srb","7h00","bra","sui","13h00","0","0","0","0")
+  createCard("02/12","sexta","srb","sui","16h00","cmr","bra","16h00","0","0","0","0")
+
+}
+
+function grupoE(){
+
+  createCard("20/11","domingo","sui","cmr","9:00","bra","srb","16h00","0","0","0","0")
+  createCard("28/11","segunda","cmr","srb","7h00","bra","sui","13h00","0","0","0","0")
+  createCard("02/12","sexta","srb","sui","16h00","cmr","bra","16h00","0","0","0","0")
+
+}
+
+function grupoF(){
+
+  createCard("20/11","domingo","sui","cmr","9:00","bra","srb","16h00","0","0","0","0")
+  createCard("28/11","segunda","cmr","srb","7h00","bra","sui","13h00","0","0","0","0")
+  createCard("02/12","sexta","srb","sui","16h00","cmr","bra","16h00","0","0","0","0")
+
+}
+
+
+
+function grupoG(){
+
+  createCard("20/11","domingo","sui","cmr","9:00","bra","srb","16:00","0","0","0","0")
+  createCard("28/11","segunda","cmr","srb","7:00","bra","sui","13:00","0","0","0","0")
+  createCard("02/12","sexta","srb","sui","16:00","cmr","bra","16:00","0","0","0","0")
+
+}
+
+function grupoH(){
+
+  createCard("20/11","domingo","sui","cmr","9:00","bra","srb","16:00","0","0","0","0")
+  createCard("28/11","segunda","cmr","srb","7:00","bra","sui","13:00","0","0","0","0")
+  createCard("02/12","sexta","srb","sui","16:00","cmr","bra","16:00","0","0","0","0")
+
+}
+
+
+window.onload = grupoG()
