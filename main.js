03e981b05ae07efebd58a3afc0264fbc9d43c14d
maikelceolin <maikelceolin@gmail.com>
2022-11-04 11:14:38 -0300

    sync

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-
 function createCard (nation1, nation2, date, day, hour, score1, score2){
   document.querySelector("main").innerHTML +=`
     <section id="cards">
@@ -26,9 +25,14 @@ function createCard (nation1, nation2, date, day, hour, score1, score2){
 function showGames (){
 let delay = 0;
 
-setTimeout(createCard, delay+=0,"bra", "srb", "24/11", "quinta", "16:00", "", "")
-setTimeout(createCard, delay+=0,"bra", "sui", "28/11", "segunda", "13:00", "", "")
-setTimeout(createCard, delay+=0,"cmr", "bra", "02/12", "sexta", "16:00", "", "")
+// jogo Brasil x Servia
+setTimeout(createCard, delay+=0,"bra", "srb", "24/11", "quinta", "16:00", "-", "-")
+
+// jogo Brasil x Suica
+setTimeout(createCard, delay+=0,"bra", "sui", "28/11", "segunda", "13:00", "-", "-")
+
+// jogo Camaroes x Brasil
+setTimeout(createCard, delay+=0,"cmr", "bra", "02/12", "sexta", "16:00", "-", "-")
 
 }
 
